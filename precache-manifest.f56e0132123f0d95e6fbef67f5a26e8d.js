c5c375b44e987bff2fe95c7c2fc83bfa05b28089
Riya Sheik <rsheik@berkeley.edu>
2021-10-28 17:58:54 -0700

    Updates

---
+++ precache-manifest.f56e0132123f0d95e6fbef67f5a26e8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533b177ba6ee1e51b432bf6431ab9bd9",
+    "url": "/reactHW/index.html"
+  },
+  {
+    "revision": "6744ae763aed296156d5",
+    "url": "/reactHW/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "c89076c55953c15f53c5",
+    "url": "/reactHW/static/css/main.b903191f.chunk.css"
+  },
+  {
+    "revision": "6744ae763aed296156d5",
+    "url": "/reactHW/static/js/2.912499d0.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "/reactHW/static/js/2.912499d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a5477a2ffe558c49ea",
+    "url": "/reactHW/static/js/3.504af603.chunk.js"
+  },
+  {
+    "revision": "c89076c55953c15f53c5",
+    "url": "/reactHW/static/js/main.c8df9468.chunk.js"
+  },
+  {
+    "revision": "c805bb8be8b29b6e38ff",
+    "url": "/reactHW/static/js/runtime-main.b39b1baf.js"
+  }
+]);
